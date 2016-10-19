2b5c1c84e650be896f2748c12186aa130fe33d38
Matthew Vass <matthew.vass@outlook.com>
2016-10-18 23:44:09 -0400

    adding examples

---
+++ read-config-xml.ps1
@@ -0,0 +1,7 @@
+﻿[xml]$config = Get-Content "D:\git-repos\powershell-examples\config.xml"
+
+Write-Output $config.OlbrConfig.RootDirectory
+
+foreach($application in $config.OlbrConfig.Applications.Application) {
+    Write-Output $application.Name
+}
---
+++ service-examples.ps1
@@ -0,0 +1,33 @@
+﻿Function isServiceRunning($serviceName) {
+    $service = Get-Service -Name $serviceName
+
+    if($service.Status -eq "Running") {
+        return $true
+    }
+
+    return $false
+}
+
+Function stopService($serviceName) {
+    Write-Output "Stopping service $serviceName ... "
+    Stop-Service -Name $serviceName -Force
+}
+
+Function startService($serviceName) {
+    Write-Output "Starting service $serviceName ... "
+    Start-Service -Name $serviceName
+}
+
+$serviceName = "Apple Mobile Device Service"
+$output = isServiceRunning $serviceName
+
+if($output -eq $true) {
+    stopService $serviceName
+}
+
+
+$output = isServiceRunning $serviceName
+
+if($output -eq $false) {
+    startService $serviceName
+}
